94d739d3c523de81016f3dde85cc6c4f1967dcbe
MartynaP93 <martyna.anna.piotrowska@gmail.com>
2024-06-06 12:02:38 +0200

    modul5

---
+++ modul5/modul5.js
@@ -0,0 +1,17 @@
+const numbers = [1, 2, 3, 4];
+
+const doubleNumbers = numbers.map((number) => number * 2);
+
+console.log(numbers);
+console.log(doubleNumbers);
+
+const students = [
+  { name: "Maria", score: 55 },
+  { name: "Kamil", score: 78 },
+  { name: "Martyna", score: 86 },
+  { name: "Konrad", score: 65 },
+];
+
+const names = students.map((student) => student.name);
+
+console.log(names);
