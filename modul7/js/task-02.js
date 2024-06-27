1169d61b953bf90904c8b6ac3203c179ff19bb03
MartynaP93 <martyna.anna.piotrowska@gmail.com>
2024-06-27 20:40:21 +0200

    modul7

--- modul7/js/task-02.js
+++ modul7/js/task-02.js
@@ -9,10 +9,25 @@ const ingredients = [
 
 const ulIngredients = document.querySelector("#ingredients");
 
-ingredients.forEach((ingredient) => {
-  const li = document.createElement("li");
-  li.textContent = ingredient;
-  li.classList.add("item");
-  ulIngredients.append(li);
-  console.log(li);
-});
+// tak mówił trener - nie działa
+// const items = ingredients.map(ingredient => {
+//   const li = document.createElement("li");
+//   li.textContent = ingredient;
+//   li.classList.add("item");
+//   console.log(li);}
+//   )
+
+// ulIngredients.append(...items)
+
+
+
+
+// to działa - moje 
+// ingredients.forEach((ingredient) => {
+//   const li = document.createElement("li");
+//   li.textContent = ingredient;
+//   li.classList.add("item");
+//   ulIngredients.append(li)
+//   console.log(li);
+// });
+
